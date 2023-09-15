d2f9f6537584831239abf33bb1111d5f957af8ec
akabishau <aleksey.kabishau@icloud.com>
2023-09-14 23:15:28 -0400

    Initial Commit

---
+++ MentorUp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MentorUp
+//
+//  Created by Aleksey Kabishau on 9/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MentorUp/MentorUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  MentorUpApp.swift
+//  MentorUp
+//
+//  Created by Aleksey Kabishau on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct MentorUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
